70c931887ea472e8e2c0f3144a4e74a6070573b0
rgopi2win <rgopi2win@gmail.com>
2018-07-23 23:09:04 +0530

    observable hands on

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import {Book} from "./book";
+import {BOOKS} from "./book-array";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,8 @@ import { Injectable } from '@angular/core';
 export class BookService {
 
   constructor() { }
+
+  getBooks():Observable<Book[]> {
+    return of(BOOKS);
+  }
 }
